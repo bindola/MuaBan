9f9a4c8f46870864c7a1320344ca68c127b37c62
Bindola <Bindola@DESKTOP-6FL7N89>
2019-03-05 23:01:26 +0700

    oke

---
+++ MuaBan/Controllers/AboutUsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+
+namespace MuaBan.Controllers
+{
+    public class AboutUsController:SurfaceController
+    {
+        private const String Patial_Path = "~/Views/Partials/AboutUs/";
+
+        public ActionResult RenderSectionAboutUs()
+        {
+            return PartialView(string.Format("{0}SectionAboutUs.cshtml", Patial_Path));
+        }
+    }
+}
